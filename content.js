d3d55ad0a439044733805b37b4bf37f9395abf0f
dannielle <dannielle.del@gmail.com>
2015-11-29 23:22:57 -0500

    wip replacing inner html with highlighted span

--- content.js
+++ content.js
@@ -4,19 +4,28 @@ function isTextNode(node) {
     return node.nodeType === 3;
 }
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
+function replaceJust(element) {
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
 
         if (isTextNode(node)) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/just/gi, '<span style="background:yellow;font-style:oblique;font-weight:bold">just</span>');
-            
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+            var originalHTML = node.innerHTML;
+            console.log("pow");
+            console.log(originalHTML);
+            console.log("text is: " + node.text);
+            if (originalHTML){
+                var replacedHTML = originalHTML.replace(/just/gi, '<span style="background:yellow;font-style:oblique;font-weight:bold">just</span>');
+                node.innerHTML = replacedHTML;
+                console.log("hit");
             }
         }
+        else if (node.childNodes.length > 0) {
+            replaceJust(node);  // Not a text node or leaf, so check it's children
+        }
     }
+}
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    replaceJust(element);
 }
